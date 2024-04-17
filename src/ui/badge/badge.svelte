eec6bb2e128378501d0623ad15705f0490beb58e
pheralb <pablohdez4340@gmail.com>
2024-04-17 14:13:06 +0100

    feat: delete tooltip + add badge & input ui components

---
+++ src/ui/badge/badge.svelte
@@ -0,0 +1,5 @@
+<span
+  class="ml-1 cursor-default rounded-md border border-neutral-200 bg-neutral-200/50 px-2 py-1 font-mono text-xs font-medium text-neutral-600 dark:border-neutral-800 dark:bg-neutral-800/60 dark:text-neutral-300"
+>
+  <slot />
+</span>
--- src/ui/button/index.ts
+++ src/ui/button/index.ts
@@ -16,7 +16,7 @@ const buttonVariants = tv({
       secondary:
         'bg-neutral-200 text-neutral-900 shadow-sm hover:bg-neutral-100/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80',
       ghost:
-        'hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
+        'hover:bg-neutral-200 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
       link: 'text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50',
       expandIcon:
         'group relative border border-neutral-200 bg-white shadow-sm hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 dark:hover:border-neutral-700/50'
