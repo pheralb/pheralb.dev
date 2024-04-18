42c8a5b73354e209ad44ab2f582087c8bbd0a845
pheralb <pablohdez4340@gmail.com>
2024-04-18 18:50:36 +0100

    feat: add markdown config & extend file extensions

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+// Markdown:
+import { mdsvex } from 'mdsvex';
+import { mdsvexOptions } from './mdsvex.options.js';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  extensions: ['.svelte', '.md'],
+  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
   kit: {
     adapter: adapter(),
     alias: {
