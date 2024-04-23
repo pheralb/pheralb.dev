5b7e8ce30adb2272f53b34f59f2058c83449d97a
pheralb <pablohdez4340@gmail.com>
2024-04-23 14:32:56 +0100

    chore: add custom plugin to add ``_blank`` target + ``rehype-slug`` & ``rehype-autolink-headings`` & ``rehype-urls``

--- mdsvex.options.js
+++ mdsvex.options.js
@@ -1,23 +1,39 @@
 import { escapeSvelte } from 'mdsvex';
 import { getHighlighter } from 'shiki';
 
+// Rehype Plugins:
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import urls from 'rehype-urls';
+
+function processUrl(url, node) {
+  if (node.tagName === 'a') {
+    node.properties.class = 'text-link';
+    if (!url.href.startsWith('/')) {
+      node.properties.target = '_blank';
+      node.properties.rel = 'noopener';
+    }
+  }
+}
+
 /** @type {import('mdsvex').MdsvexOptions} */
 export const mdsvexOptions = {
   extensions: ['.md'],
   layout: {
     _: './src/components/mdsvex/md-components.svelte'
   },
+  rehypePlugins: [[urls, processUrl], rehypeSlug, rehypeAutolinkHeadings],
   highlight: {
     highlighter: async (code, lang = 'text') => {
       const highlighter = await getHighlighter({
         themes: ['slack-ochin', 'poimandres'],
-        langs: ['javascript', 'typescript', 'bash', 'json'],
+        langs: ['javascript', 'typescript', 'bash', 'json']
       });
       await highlighter.loadLanguage('javascript', 'typescript', 'bash', 'json');
       const html = escapeSvelte(
         highlighter.codeToHtml(code, {
           lang,
-          theme: 'poimandres',
+          theme: 'poimandres'
         })
       );
       return `{@html \`${html}\` }`;
