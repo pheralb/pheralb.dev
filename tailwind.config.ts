a9cd1cd042729af30e4b6aa562a4111e5baad141
pheralb <pablohdez4340@gmail.com>
2024-06-25 08:51:02 +0100

    feat: add spotlight-badge UI component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,19 @@ const config: Config = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
         mono: ['GeistMono', ...defaultTheme.fontFamily.mono],
         gambarino: ['Gambarino', ...defaultTheme.fontFamily.sans]
+      },
+      animation: {
+        shine: 'shine 2s linear infinite'
+      },
+      keyframes: {
+        shine: {
+          from: {
+            backgroundPosition: '0 0'
+          },
+          to: {
+            backgroundPosition: '-200% 0'
+          }
+        }
       }
     }
   }
