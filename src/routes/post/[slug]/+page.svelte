f64a46c11a783c9f1c34eeda960455bbe29bf275
pheralb <pablohdez4340@gmail.com>
2024-07-22 11:47:14 +0100

    feat: improve post page with new badges

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -2,7 +2,7 @@
   import Badge from '@/ui/badge/badge.svelte';
   import { routeAnimation } from '@/ui/shared';
   import { cn, formatDate } from '@/utils';
-  import { ArrowUpRight, ClockIcon } from 'lucide-svelte';
+  import { ArrowUpRight, ClockIcon, TagIcon, TriangleAlertIcon } from 'lucide-svelte';
 
   export let data;
 </script>
@@ -21,37 +21,43 @@
     </h2>
     <p class="text-neutral-800 dark:text-neutral-400">{data.meta.description}</p>
   </div>
-  <div
-    class="flex items-center justify-between border-b border-neutral-300 py-4 text-sm dark:border-neutral-800"
-  >
-    <div class="flex items-center space-x-3">
-      <Badge>{data.meta.category}</Badge>
-      <time
-        class="flex items-center space-x-2 text-neutral-800 dark:text-neutral-400"
-        datetime={data.meta.date}
+  <div class="flex flex-col space-y-2 border-b border-neutral-300 py-5 dark:border-neutral-800">
+    <div class="flex items-center justify-between text-sm">
+      <div class="flex items-center space-x-[6px]">
+        <Badge>
+          <TagIcon size={12} />
+          <span>{data.meta.category}</span>
+        </Badge>
+        <Badge>
+          <ClockIcon size={12} />
+          <time datetime={data.meta.date}>
+            {formatDate(data.meta.date)}
+          </time>
+        </Badge>
+      </div>
+      <a
+        href={`https://github.com/pheralb/pheralb.dev/blob/main/src/posts/${data.slug}.md`}
+        class="flex items-center space-x-1 text-neutral-600 transition-colors duration-200 ease-in-out hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-white"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <ClockIcon size="14" />
-        <span class="font-mono">{formatDate(data.meta.date)}</span>
-      </time>
+        <span>Edit on GitHub</span>
+        <ArrowUpRight size="14" />
+      </a>
     </div>
-    <a
-      href={`https://github.com/pheralb/pheralb.dev/blob/main/src/posts/${data.slug}.md`}
-      class="flex items-center space-x-1 text-neutral-600 transition-colors duration-200 ease-in-out hover:text-neutral-900 dark:text-neutral-400 dark:hover:text-white"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <span>Edit on GitHub</span>
-      <ArrowUpRight size="14" />
-    </a>
+    {#if data.meta.writing}
+      <div
+        class="flex items-center gap-2 rounded-md border border-neutral-300 bg-neutral-200/40 p-2 text-neutral-700 dark:border-neutral-800 dark:bg-neutral-800/60 dark:text-neutral-300 text-sm"
+      >
+        <TriangleAlertIcon size={16} />
+        <p class="text-pretty">
+          This post is a work in progress. It may be incomplete, contain errors, or be subject to
+          breaking changes.
+        </p>
+      </div>
+    {/if}
   </div>
-  {#if data.meta.writing}
-    <div class="flex flex-col space-y-2 border-l border-neutral-300 pl-4 dark:border-neutral-800">
-      <p class="text-pretty text-sm text-neutral-700 dark:text-neutral-400">
-        🚧 This post is a work in progress. It may be incomplete, contain errors, or be subject to
-        breaking changes.
-      </p>
-    </div>
-  {/if}
+
   <article
     class={cn(
       'pb-5',
@@ -60,7 +66,7 @@
       'prose-a:decoration-neutral-400 prose-a:decoration-dotted prose-a:decoration-[1.5px] prose-a:underline-offset-[6px] hover:prose-a:opacity-80 dark:prose-a:decoration-neutral-400',
       'prose-pre:my-3',
       'prose-ol:mb-3 prose-ul:mb-3',
-      'prose-inline-code:rounded prose-inline-code:font-mono prose-inline-code:p-[2px] prose-inline-code:border prose-inline-code:border-neutral-300 prose-inline-code:bg-neutral-200/50 prose-inline-code:dark:border-neutral-800 prose-inline-code:dark:bg-neutral-800/50'
+      'prose-inline-code:rounded prose-inline-code:border prose-inline-code:border-neutral-300 prose-inline-code:bg-neutral-200/50 prose-inline-code:p-[2px] prose-inline-code:font-mono prose-inline-code:dark:border-neutral-800 prose-inline-code:dark:bg-neutral-800/50'
     )}
   >
     <svelte:component this={data.content} />
--- src/ui/badge/badge.svelte
+++ src/ui/badge/badge.svelte
@@ -1,5 +1,14 @@
+<script lang="ts">
+  import { cn } from '@/utils';
+
+  export let extraClasses: string = '';
+</script>
+
 <span
-  class="cursor-default rounded-md inline-flex border border-neutral-300 bg-neutral-200/50 px-2 py-1 font-mono text-xs font-medium text-neutral-600 dark:border-neutral-800 dark:bg-neutral-800/60 dark:text-neutral-300"
+  class={cn(
+    'inline-flex cursor-default items-center space-x-2 rounded-md border border-neutral-300 bg-neutral-200/50 px-2 py-1 font-mono text-xs font-medium text-neutral-700 dark:border-neutral-800 dark:bg-neutral-800/60 dark:text-neutral-300',
+    extraClasses
+  )}
 >
   <slot />
 </span>
