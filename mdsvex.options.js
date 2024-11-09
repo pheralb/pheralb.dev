1f0d2be8bd579ed855e6602f8cd6d5cbb50c14c6
pheralb <pablohdez4340@gmail.com>
2024-11-09 18:55:00 +0000

    feat: add ``addCopyButton`` transformer for Shiki

--- mdsvex.options.js
+++ mdsvex.options.js
@@ -1,4 +1,5 @@
 import { escapeSvelte } from 'mdsvex';
+import { h } from 'hastscript';
 
 // Rehype Plugins:
 import rehypeSlug from 'rehype-slug';
@@ -12,6 +13,7 @@ import { createHighlighter, makeSingletonHighlighter } from 'shiki/bundle/web';
 // Highlighter Singleton:
 const getHighlighter = makeSingletonHighlighter(createHighlighter);
 
+// Open external links in new tab:
 function processUrl(url, node) {
   if (node.tagName === 'a') {
     node.properties.class = 'text-link';
@@ -22,6 +24,7 @@ function processUrl(url, node) {
   }
 }
 
+// Customize TOC:
 const customizeTOC = (toc) => {
   try {
     const { children } = toc;
@@ -51,6 +54,91 @@ const customizeTOC = (toc) => {
   };
 };
 
+// Copy to cliboard button:
+function addCopyButton(options = {}) {
+  const toggleMs = options.toggle || 2000;
+  return {
+    name: 'shiki-copy-button',
+    pre(node) {
+      node.properties = node.properties || {};
+      node.properties.style = 'position: relative;';
+
+      let iconSize = 16;
+      let iconStrokeSize = 1.95;
+
+      const button = h(
+        'button',
+        {
+          class: 'shiki-copy-button-styles',
+          title: 'Copy code',
+          'data-code': this.source,
+          onclick: `
+            navigator.clipboard.writeText(this.dataset.code);
+            this.classList.add('copied');
+            setTimeout(() => this.classList.remove('copied'), ${toggleMs});
+          `
+        },
+        [
+          // Copy icon:
+          h('span', { class: 'icon-copy-container' }, [
+            h(
+              'svg',
+              {
+                xmlns: 'http://www.w3.org/2000/svg',
+                width: iconSize,
+                height: iconSize,
+                viewBox: '0 0 24 24',
+                fill: 'none',
+                title: 'Copy code',
+                stroke: 'currentColor',
+                'stroke-width': iconStrokeSize,
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                class: 'icon-copy shiki-copy-icon-colors'
+              },
+              [
+                h('rect', {
+                  width: '14',
+                  height: '14',
+                  x: '8',
+                  y: '8',
+                  rx: '2',
+                  ry: '2'
+                }),
+                h('path', {
+                  d: 'M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2'
+                })
+              ]
+            )
+          ]),
+          // Copied icon:
+          h('span', { class: 'icon-copied-container' }, [
+            h(
+              'svg',
+              {
+                xmlns: 'http://www.w3.org/2000/svg',
+                width: iconSize,
+                height: iconSize,
+                viewBox: '0 0 24 24',
+                title: 'Copied!',
+                fill: 'none',
+                stroke: 'currentColor',
+                'stroke-width': iconStrokeSize,
+                'stroke-linecap': 'round',
+                'stroke-linejoin': 'round',
+                class: 'icon-copied shiki-copy-icon-colors'
+              },
+              [h('path', { d: 'M18 6 7 17l-5-5' }), h('path', { d: 'm22 10-7.5 7.5L13 16' })]
+            )
+          ])
+        ]
+      );
+
+      node.children.push(button);
+    }
+  };
+}
+
 /** @type {import('mdsvex').MdsvexOptions} */
 export const mdsvexOptions = {
   extensions: ['.md'],
@@ -75,16 +163,16 @@ export const mdsvexOptions = {
     highlighter: async (code, lang = 'text') => {
       const highlighter = await getHighlighter({
         themes: ['github-light', 'github-dark'],
-        langs: ['javascript', 'typescript', 'bash', 'json', 'jsx', 'css', 'tsx'],
+        langs: ['javascript', 'typescript', 'bash', 'json', 'jsx', 'css', 'tsx']
       });
       const html = escapeSvelte(
         highlighter.codeToHtml(code, {
           lang,
-          keepBackground: false,
           themes: {
             light: 'github-light',
             dark: 'github-dark'
-          }
+          },
+          transformers: [addCopyButton()]
         })
       );
       return `{@html \`${html}\` }`;
