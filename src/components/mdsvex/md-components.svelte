74039dbe800738b60ec24674882a277f55a69e3d
pheralb <pablohdez4340@gmail.com>
2024-11-09 18:57:13 +0000

    fix: ``children`` property

--- src/components/mdsvex/md-components.svelte
+++ src/components/mdsvex/md-components.svelte
@@ -1,11 +1,8 @@
 <script>
-  /** @type {{children?: import('svelte').Snippet}} */
-  let { children } = $props();
-</script>
-
-<script lang="ts" module>
   import { img } from '.';
   export { img };
+
+  export let children;
 </script>
 
 {@render children?.()}
